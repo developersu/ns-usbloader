94b0c764f179eabe6aa72cc2ba308960fe203606
Dmitry Isaenko <developer.su@gmail.com>
2019-02-19 04:37:21 +0300

    v0.2.1 pro-active progress bar added

--- src/main/java/nsusbloader/NSLMain.java
+++ src/main/java/nsusbloader/NSLMain.java
@@ -12,7 +12,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class NSLMain extends Application {
-    public static final String appVersion = "v0.2";
+    public static final String appVersion = "v0.2.1";
     @Override
     public void start(Stage primaryStage) throws Exception{
 
