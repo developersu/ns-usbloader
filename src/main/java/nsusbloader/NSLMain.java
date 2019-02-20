bed91bdd866ab27d4e9ec9a312ab173ab5c3f973
Dmitry Isaenko <developer.su@gmail.com>
2019-02-20 16:39:30 +0300

    v0.2.2 fixing missed openJFX dependencies

--- src/main/java/nsusbloader/NSLMain.java
+++ src/main/java/nsusbloader/NSLMain.java
@@ -12,7 +12,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class NSLMain extends Application {
-    public static final String appVersion = "v0.2.1";
+    public static final String appVersion = "v0.2.2";
     @Override
     public void start(Stage primaryStage) throws Exception{
 
