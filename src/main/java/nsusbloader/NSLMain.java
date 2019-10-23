ab6b3e020695e08b73fbe8273b867ed5523ad266
Dmitry Isaenko <developer.su@gmail.com>
2019-10-24 01:58:56 +0300

    Update checkboxes in dark theme

--- src/main/java/nsusbloader/NSLMain.java
+++ src/main/java/nsusbloader/NSLMain.java
@@ -12,7 +12,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class NSLMain extends Application {
-    public static final String appVersion = "v0.8.1";
+    public static final String appVersion = "v0.8.2";
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/NSLMain.fxml"));
